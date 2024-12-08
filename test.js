0a1ef8e6964459a68c57f1ab9b6d7cb14dd8305d
JJules15 <mjules@bu.edu>
2024-12-08 12:29:47 -0500

    Initial commit

---
+++ test.js
@@ -0,0 +1,17 @@
+console.log("Hello World!\nThis is the second line")
+console.log(typeof 4.5)
+
+//This is a binding
+let caught = 5*5
+var name = "Marino Jules"
+const firstName = "Marino"
+
+//prompt("Enter password: ")
+
+console.log(Math.max(2, 4) + 100)
+
+let theNumber = Number(prompt("Pick a number: "));
+if (!Number.isNaN(theNumber))
+{
+    console.log("Your number is the square root of " + theNumber + theNumber);
+}
